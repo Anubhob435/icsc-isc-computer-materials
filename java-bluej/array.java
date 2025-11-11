09b65c0afda65ccc98d0b2f575036266e4231ee6
Anubhob <anubhob493@gmail.com>
2025-11-11 22:27:45 +0530

    java code

---
+++ java-bluej/array.java
@@ -0,0 +1,17 @@
+class array{
+    public static void main(String [] args){
+        
+        String [] arr = { "apple","banana","pine", "fruit"};
+            
+            for(int i = 0; i<arr.length; i++){
+                String x =arr[i];
+                System.out.println(x); 
+                
+                
+                for(int j = 0; j<x.length(); j++){
+                    System.out.println(
+                }
+            }
+            
+    }
+}
