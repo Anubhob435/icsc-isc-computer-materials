ff64bbbb0f5d73a39be3145e2699288bbb58789b
Anubhob <anubhob493@gmail.com>
2025-12-01 14:42:54 +0530

    clean repo

--- java-bluej/CreateFile.java
+++ java-bluej/CreateFile.java
@@ -5,7 +5,7 @@ public class CreateFile {
     public static void main(String[] args) throws IOException {
         FileWriter writer = new FileWriter("example.txt", true);
         writer.write("Hello from Java!\n");
-        writer.write("This is a text file created without try-catch.");
+        writer.write("This is a text file created without try-catch again.");
         writer.close();
         System.out.println("example.txt created successfully!");
     }
